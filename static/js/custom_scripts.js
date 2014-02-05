4dc7e2d5cb344f56ca2f5b8632bf4afecea5f5d5
Florent Lebreton <florentlebreton@free.fr>
2014-02-05 13:57:57 +0100

    Better management of fixed sidebar width

--- static/js/custom_scripts.js
+++ static/js/custom_scripts.js
@@ -2,8 +2,10 @@
     function onScroll(e) {
       var sidebar = document.getElementById('sidebar-container-fixed');  
       if (window.scrollY > 0) {
+        sidebar.style.width = sidebar.offsetWidth + 'px'
         sidebar.classList.add('sticky');
       } else {
+        sidebar.style.width = 'auto'
         sidebar.classList.remove('sticky');
       }
     }
